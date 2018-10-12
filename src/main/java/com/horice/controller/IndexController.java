f7f30f98f7cece304903a28cc206ef393c93bb8c
horice <864742615@qq.com>
2018-10-12 14:58:43 +0800

    配置 freemarker 为 bean 配置，标签配置出现乱码，未找到配置方式

--- src/main/java/com/horice/controller/IndexController.java
+++ src/main/java/com/horice/controller/IndexController.java
@@ -2,14 +2,10 @@ package com.horice.controller;
 
 import com.horice.entity.Student;
 import com.horice.service.StudentService;
-import com.horice.service.impl.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +20,22 @@ public class IndexController {
     //@ResponseBody
     public String index(@RequestParam(value = "id",required = false,defaultValue = "1") Integer id, Student student, Map<String,Object> map){
 
-
-        Student student1 = studentService.getStudentById(id);
+        /*Student student1 = studentService.getStudentById(id);
         System.out.println(student1);
         System.out.println(student);
-        System.out.println(id);
+        System.out.println(id);*/
         List list = new ArrayList<Integer>();
         list.add(1);
         list.add(2);
         list.add(3);
+        map.put("list",list);
         map.put("title","测试标题");
+        System.out.println("测试标题");
         return "index";
+    }
+
+    @RequestMapping("test")
+    public String test(){
+        return "test";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/index.ftl
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Title</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+spring mvc index.jsp
+标题 ${title}
+<#list list as item>
+    ${item}
+</#list>
+</body>
+</html>
